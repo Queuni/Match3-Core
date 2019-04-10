3aff0e5050a0f4b3c882183380fbb9f353077fba
Queuni <Queuni@users.noreply.github.com>
2019-04-10 10:06:49 +0000

    Refactor utils to use a single source of truth for default values

--- lib/helpers.py
+++ lib/helpers.py
@@ -4,3 +4,5 @@
 # Remove deprecated CLI flag and update docs to use the new option
 
 # Refactor exports so the public API is clearer and easier to use
+
+# Implement a small in-memory cache for the config to avoid re-reading
