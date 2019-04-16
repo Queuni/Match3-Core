ef517e2f5f7ebc5a0c4740c81e34bbf293d7e6a2
Queuni <Queuni@users.noreply.github.com>
2019-04-16 17:57:13 +0000

    Clean up unused imports and fix formatting to match the project style guide

--- src/config.py
+++ src/config.py
@@ -4,3 +4,5 @@
 # Clean up unused imports and fix formatting to match the project style guide
 
 # Fix incorrect type hint that was causing mypy to fail in CI
+
+# Improve test coverage for the helpers module to above 90%
