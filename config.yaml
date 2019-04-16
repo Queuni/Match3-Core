d5e939f870795ddf52756e33693b904d0dd183ae
Queuni <Queuni@users.noreply.github.com>
2019-04-16 09:16:37 +0000

    Remove redundant check that was already covered by the validator

--- config.yaml
+++ config.yaml
@@ -1,2 +1,4 @@
 # config
 
+
+# Simplify the config merge logic so overrides are predictable
--- src/main.py
+++ src/main.py
@@ -4,3 +4,5 @@
 # Adjust the default concurrency limit based on load test results
 
 # Remove hardcoded credentials and move to env-based configuration
+
+# Simplify error messages so they are actionable for the end user
