e26d7eaac9b4a92190f407e348c726f026118973
Queuni <Queuni@users.noreply.github.com>
2019-06-05 11:19:38 +0000

    Bump dependency to get the security fix for the reported CVE

--- src/config.py
+++ src/config.py
@@ -24,3 +24,5 @@
 # Correct the default path used when no config file is specified
 
 # Remove deprecated CLI flag and update docs to use the new option
+
+# Correct the docstring to match the actual behavior of the function
